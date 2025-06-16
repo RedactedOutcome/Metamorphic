ecdb1df39f9678c62cf4538a6a87afc816d16468
1WeirdDev <the1weirddev@gmail.com>
2025-06-16 12:20:57 -0500

    Calling COmponent Methods

--- include/Metamorphic/Core/Objects/GameObject.h
+++ include/Metamorphic/Core/Objects/GameObject.h
@@ -8,11 +8,19 @@ namespace Metamorphic{
         ~GameObject()noexcept;
 
         void Awake()noexcept;
+        void Start()noexcept;
         void Update()noexcept;
         void LateUpdate()noexcept;
         void Draw()noexcept;
         void LateDraw()noexcept;
     public:
-        
+        template<typename T, typename... Args>
+        std::unique_ptr<T>& CreateComponent(Args&& args)noexcept{
+            m_Components.emplace_back(std::make_unique<T>(std::forward<Args>(args)));
+            return m_Components[m_Components.size() - 1];
+        }
+    private:
+        bool m_Enabled = true;
+        std::vector<std::unique_ptr<Component>> m_Components;
     };
 }
--- src/Core/Objects/GameObject.cpp
+++ src/Core/Objects/GameObject.cpp
@@ -5,9 +5,34 @@ namespace Metamorphic{
     GameObject::GameObject()noexcept{}
     GameObject::~GameObject()noexcept{}
 
-    void GameObject::Awake()noexcept{}
-    void GameObject::Update()noexcept{}
-    void GameObject::LateUpdate()noexcept{}
-    void GameObject::Draw()noexcept{}
-    void GameObject::LateDraw()noexcept{}
+    void GameObject::Awake()noexcept{
+        for(size_t i = 0; i < m_Components.size(); i++){
+            m_Components[i]->Awake();
+        }
+    }
+    void GameObject::Start()noexcept{
+        for(size_t i = 0; i < m_Components.size(); i++){
+            m_Components[i]->Awake();
+        }
+    }
+    void GameObject::Update()noexcept{
+        for(size_t i = 0; i < m_Components.size(); i++){
+            m_Components[i]->Update();
+        }
+    }
+    void GameObject::LateUpdate()noexcept{
+        for(size_t i = 0; i < m_Components.size(); i++){
+            m_Components[i]->LateUpdate();
+        }
+    }
+    void GameObject::Draw()noexcept{
+        for(size_t i = 0; i < m_Components.size(); i++){
+            m_Components[i]->Draw();
+        }
+    }
+    void GameObject::LateDraw()noexcept{
+        for(size_t i = 0; i < m_Components.size(); i++){
+            m_Components[i]->LateDraw();
+        }
+    }
 }
