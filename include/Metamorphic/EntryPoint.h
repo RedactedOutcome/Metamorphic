6fb16199067a6fbc3bdbf5014d6e845e164efc81
1WeirdDev <the1weirddev@gmail.com>
2025-06-28 15:50:14 -0500

    Update

--- include/Metamorphic/EntryPoint.h
+++ include/Metamorphic/EntryPoint.h
@@ -1,6 +1,13 @@
 /// @brief This file is not included in the engine build process. This file stands as a
 /// Simple main function that simple starts and executes the Metamorphic Engine and project 
+
+#include "Metamorphic/Core/PlatformDetection.h"
+
+#ifdef METAMORPHIC_PLATFORM_WINDOWS
 int main(int argc, char** argv){
+#else
+#error Metamorphic No Supported platform
+#endif
     using namespace Metamorphic;
     std::unique_ptr<Application> application(CreateApplication());
     application->Run();
