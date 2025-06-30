db958a3ad906ba3e6802d3a4d8c242b296dd1c4a
1WeirdDev <the1weirddev@gmail.com>
2025-06-29 22:04:51 -0500

    Update

---
+++ include/Metamorphic/Platform/Renderers/MetalRenderer.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "Metamorphic/Rendering/IRenderAPI.h"
+namespace Metamorphic{
+    class MetalRenderer : public IRenderAPI{
+    public:
+        
+    };
+}
---
+++ src/Platform/Renderers/MetalRenderer.cpp
@@ -0,0 +1,6 @@
+#include "Metamorphic/pch.h"
+#include "Platform/Renderers/MetalRenderer.h"
+
+namespace Metamorphic{
+    
+}
