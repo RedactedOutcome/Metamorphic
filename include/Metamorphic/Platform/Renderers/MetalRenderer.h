96378d388234f0114b3cf4350e0dbd0cb3193af8
1WeirdDev <the1weirddev@gmail.com>
2025-06-30 20:44:11 -0500

    Preparing Android Support

--- include/Metamorphic/Platform/Renderers/MetalRenderer.h
+++ include/Metamorphic/Platform/Renderers/MetalRenderer.h
@@ -1,9 +1,18 @@
 #pragma once
 
 #include "Metamorphic/Rendering/IRenderAPI.h"
+
 namespace Metamorphic{
     class MetalRenderer : public IRenderAPI{
     public:
-        
+        MetalRenderer(IWindow* window)noexcept;
+        ~MetalRenderer()noexcept override;
+
+        RenderAPIError Init()noexcept override;
+        RenderAPIError Shutdown()noexcept override;
+
+        void PrepareScreen() noexcept override;
+        void ClearDepthBuffers()noexcept override;
+        void Update()noexcept override;
     };
 }
--- src/Platform/Renderers/MetalRenderer.cpp
+++ src/Platform/Renderers/MetalRenderer.cpp
@@ -1,6 +1,15 @@
 #include "Metamorphic/pch.h"
 #include "Platform/Renderers/MetalRenderer.h"
+#include "Core/Logger.h"
 
 namespace Metamorphic{
-    
+    MetalRenderer::MetalRenderer(IWindow* window)noexcept: IRenderAPI(window){}
+    MetalRenderer::~MetalRenderer()noexcept {}
+
+    RenderAPIError MetalRenderer::Init()noexcept{return RenderAPIError::None;}
+    RenderAPIError MetalRenderer::Shutdown()noexcept{return RenderAPIError::None;}
+
+    void MetalRenderer::PrepareScreen()noexcept{}
+    void MetalRenderer::ClearDepthBuffers()noexcept{}
+    void MetalRenderer::Update()noexcept{}
 }
